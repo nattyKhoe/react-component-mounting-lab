7e53252268ac74b52a02afae78724baf05a566e7
nattyKhoe <natasha.kusuma@outlook.com>
2022-08-27 23:30:27 +1000

    done

--- src/Timer.js
+++ src/Timer.js
@@ -5,8 +5,14 @@ class Timer extends Component {
     time: 0,
     color: "#" + Math.floor(Math.random() * 16777215).toString(16)
   };
+  
+  componentDidMount () {
+    this.interval = setInterval(this.clockTick,1000);
+  };
 
-  // add your code here
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  };
 
   render() {
     const { time, color } = this.state;
